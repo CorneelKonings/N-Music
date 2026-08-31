7df5d78be16226d60aaac2f51c063e3662c056c9
muwmx <alekseypered@gmail.com>
2026-08-31 18:18:04 +0300

    feat(ci,release): add artifact reuse logic for Build APKs workflow in release pipeline

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,8 +51,8 @@ android {
     applicationId = "re.muwmix.yumaplayer"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.0.0-Beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
